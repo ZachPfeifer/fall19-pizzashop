e43d8f346565a1779915f204132a43dcd7ae6c3a
Zach Pfeifer <mr.zman1@yahoo.com>
2019-08-22 15:21:58 -0600

    Animal Model Basics

--- app/Services/PizzaService.js
+++ app/Services/PizzaService.js
@@ -13,21 +13,47 @@ let _state = {
 export default class PizzaService {
   deleteTopping(pizzaIndex, toppingIndex) {
     _state.pizzas[pizzaIndex].toppings.splice(toppingIndex, 1)
+    //extra for savedPizzas()
+    this.savedPizzas()
   }
   deletePizza(index) {
     _state.pizzas.splice(index, 1)
+    //extra for savedPizzas()
+    this.savedPizzas()
   }
   addTopping(newTopping, pizzaIndex) {
     _state.pizzas[pizzaIndex].toppings.push(newTopping)
+    //extra for savedPizzas()
+    this.savedPizzas()
   }
   addPizza(newPizza) {
     _state.pizzas.push(new Pizza(newPizza))
     console.log(_state.pizzas)
+    //extra for savedPizzas()
+    this.savedPizzas()
   }
   constructor() {
     console.log("Howdy from pizza service........")
+    this.savedPizzas()
+
   }
   get Pizza() {
     return _state.pizzas.map(pizza => new Pizza(pizza))
   }
+
+  // extra 
+
+  //load pizza
+  loadPizzas() {
+    let savedPizzas = JSON.parse(localStorage.getItem("pizzas"))
+    if (savedPizzas) {
+      _state.pizzas = savedPizzas
+    }
+  }
+
+  //save pizza for next load
+  savedPizzas() {
+    localStorage.setItem("pizzas", JSON.stringify(_state.pizzas))
+  }
+
 }
