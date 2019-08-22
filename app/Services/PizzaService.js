eead5a2674e0253b5132ab07b9e9913d410b0f88
Darryl Kilzer <darrylkilzer@gmail.com>
2019-08-22 10:53:06 -0600

    Nick

--- app/Services/PizzaService.js
+++ app/Services/PizzaService.js
@@ -1,9 +1,20 @@
 import Pizza from "../Models/Pizza.js";
 
-let _pizza = new Pizza()
+let _state = {
+  pizzas: [new Pizza({
+    name: "Pepperoni",
+    size: "XL",
+    toppings: ["cheese", "sauce", "pepperoni"],
+    crust: "pan",
+    price: 2
+  })]
+}
 
 export default class PizzaService {
   constructor() {
     console.log("Howdy from pizza service........")
+  }
+  get Pizza() {
+    return _state.pizzas.map(pizza => new Pizza(pizza))
   }
 }
